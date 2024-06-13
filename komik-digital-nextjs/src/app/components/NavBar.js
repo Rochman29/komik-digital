7146a331d32683310f28c3edb74d44a4012809f3
Rochman29 <117629341+Rochman29@users.noreply.github.com>
2024-06-13 21:14:59 +0700

    Update NavBar.js

--- komik-digital-nextjs/src/app/components/NavBar.js
+++ komik-digital-nextjs/src/app/components/NavBar.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <div className="bg-[#996633] h-16 flex flex-row items-center text-[beige] z-[1]">
-      <div className="flex flex-row justify-between min-w-full px-10">
+      <div className="flex flex-row justify-between min-w-full px-5">
         <div className="">
           <div>Digital Komik</div>
         </div>
